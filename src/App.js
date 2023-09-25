1eafba7e6b5f5a0901fb971920e3305cc66537a6
Praveen Patro <praveen.patro911@gmail.com>
2023-09-25 23:33:52 +0530

    Update generate qr

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Qrpage from './Components/Qr_generator/Qr_page';
-
+import Qrshow from './Components/Qr_generator/Qr_show';
 
 
 function App() {
@@ -19,6 +19,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/Qr' element={<Qrpage/>} />
+        <Route path='/Qrshow' element={<Qrshow/>} />
        
       </Routes>
      </Router>
